d45c7b90fbf30620412b975916461849c1888d75
daya2940 <sauuit2940@gmail.com>
2022-12-21 19:20:26 +0530

    Expanding images initial

---
+++ script.js
@@ -0,0 +1,17 @@
+const panels = document.querySelectorAll(".panel");
+console.log(panels);
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+      panel.classList.remove("active");
+    });
+  });
+}
